e2450b268ea41a7f66d0345947d00cdb773f25d2
Seungjik-Lee <75673900+Seungjik-Lee@users.noreply.github.com>
2021-01-28 15:55:54 +0900

    Add files via upload

---
+++ 20210126/src/Ex02.java
@@ -0,0 +1,16 @@
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		/* Ex02 */
+		System.out.println("2 + 5 = " + 2 + 5);
+		System.out.println("2 + 5 = " + (2 + 5));
+
+		/* Ex03 */
+		System.out.println(12);
+		System.out.println("1" + "2");
+		System.out.println("1" + 2);
+		System.out.println(1 + "2");
+		System.out.println("" + 1 + 2);
+	}
+}
---
+++ 20210126/src/FiJavaProm.java
@@ -0,0 +1,9 @@
+
+public class FiJavaProm {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("안녕하세요 java 입니다.");
+	}
+
+}
