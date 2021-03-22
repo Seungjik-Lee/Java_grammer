3de1bc3c5de0f0180b654faa8f9118cdc18ac7ea
dltmdwlr <dltmdwlr@gmail.com>
2021-03-22 11:32:28 +0900

    커밋

---
+++ 20210322/src/Ex03.java
@@ -0,0 +1,10 @@
+
+public class Ex03 {
+public static void main(String[] args) {
+	Cake cakes[] = {new Cake(), new CheezeCake(), new Cake()};
+	
+	for(int i = 0; i < cakes.length; i++)
+		cakes[i].yummy();
+	}
+}
+
