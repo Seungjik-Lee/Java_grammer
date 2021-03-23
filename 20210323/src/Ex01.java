f7610794b97d10cb313946ddb63f33a92a7f3a0d
dltmdwlr <dltmdwlr@gmail.com>
2021-03-23 09:36:31 +0900

    커밋

---
+++ 20210323/src/Ex01.java
@@ -0,0 +1,17 @@
+interface A{
+	public void doA(String a);
+}
+
+public class Ex01 implements A{
+	
+	public static void main(String[] args) {
+		Ex01 ex01 = new Ex01();
+		ex01.doA("test");
+	}
+
+	@Override
+	public void doA(String a) {
+		// TODO Auto-generated method stub
+		System.out.println("doA 함수입니다.");
+	}
+}
